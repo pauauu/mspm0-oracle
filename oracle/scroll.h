9ba083fdf26c8b63d4fc8af0fd3b0a49a8dcbd4f
alosnakniyo <143167407+alosnakniyo@users.noreply.github.com>
2025-07-23 23:50:52 -0500

    Updated scroll.h

--- oracle/scroll.h
+++ oracle/scroll.h
@@ -1,8 +1,18 @@
-#ifndef scroll_h
-#define scroll_h
+#ifndef SCROLL_H
+#define SCROLL_H
+
+#include <stdint.h> // standard integer library
 
 void display_char(uint8_t display_idx, char ch);
 void update_displays();
 void scroller();
+
+void show_next_string(void);
+void show_full_msg(int idx);
+
+// globals
+extern volatile uint32_t press_len;
+extern unsigned int scroll_idx;
+extern unsigned int string_idx;
 
 #endif
